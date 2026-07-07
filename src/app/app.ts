4d78556e06a8bdb5f87ca9a939c64eaf9538807f
Felix Wagner <post@felixwagner.eu>
2026-07-07 16:59:08 +0200

    refactor: replace font mixin by font-size-wght mixin to reduce css orders in classes to prevent scss to be too large for compiling on build

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NewSurveyComponent } from './new-survey/new-survey.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NewSurveyComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
