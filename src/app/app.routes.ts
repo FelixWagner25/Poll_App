8782b3c57d6bddb941c3ab391dfa0bb26674520d
Felix Wagner <post@felixwagner.eu>
2026-07-11 20:16:44 +0200

    feat: style head information section in survey results component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NewSurveyComponent } from './new-survey/new-survey.component';
+import { SurveyResultsComponent } from './survey-results/survey-results.component';
 
 export const routes: Routes = [
-  { path: '', component: NewSurveyComponent }, //HomeComponent },
+  //{ path: '', component: HomeComponent },
+  { path: '', component: SurveyResultsComponent },
   { path: 'new-survey', component: NewSurveyComponent },
 ];
