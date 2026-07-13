724085f5a5f7072e30a0d8284f5a6f6245c91c60
Felix Wagner <post@felixwagner.eu>
2026-07-13 12:31:41 +0200

    feat: add icons to primary button and scrollbar to survey-list-wrap in home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { NewSurveyComponent } from './new-survey/new-survey.component';
 import { SurveyResultsComponent } from './survey-results/survey-results.component';
 
 export const routes: Routes = [
-  //{ path: '', component: HomeComponent },
-  { path: '', component: SurveyResultsComponent },
+  { path: '', component: HomeComponent },
   { path: 'new-survey', component: NewSurveyComponent },
+  { path: 'survey-results', component: SurveyResultsComponent },
 ];
