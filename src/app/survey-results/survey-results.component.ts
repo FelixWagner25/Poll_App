23396326ad3c539c9d7c07f964122e215a870b4c
Felix Wagner <post@felixwagner.eu>
2026-07-13 10:46:06 +0200

    feat: add routerlink and small stlye changes to survey results component

--- src/app/survey-results/survey-results.component.ts
+++ src/app/survey-results/survey-results.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-survey-results',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './survey-results.component.html',
   styleUrl: './survey-results.component.scss',
 })
-export class SurveyResultsComponent {
-
-}
+export class SurveyResultsComponent {}
