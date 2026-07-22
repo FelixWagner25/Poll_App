a6d4a38c58b39727a30e598a85a3d6cecf8814fb
Felix Wagner <post@felixwagner.eu>
2026-07-22 19:31:38 +0200

    feat: adding of additional answers for additional questions in survey form

--- src/app/new-question/new-question.component.ts
+++ src/app/new-question/new-question.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NewAnswerComponent } from '../new-answer/new-answer.component';
 
 @Component({
   selector: 'app-new-question',
-  imports: [],
+  imports: [NewAnswerComponent],
   templateUrl: './new-question.component.html',
   styleUrl: './new-question.component.scss',
 })
 export class NewQuestionComponent {
+  additionalQuestionIndices = signal<number[]>([]);
+  additionalAnswerIndices = signal<number[]>([]);
 
+  addNextAnswer() {
+    let nextIndex = this.additionalAnswerIndices().length;
+    if (nextIndex >= 4) return;
+    this.additionalAnswerIndices.update((array) => [...array, nextIndex]);
+  }
+
+  deleteAnswer(index: number) {
+    this.additionalAnswerIndices().splice(index, 1);
+  }
 }
