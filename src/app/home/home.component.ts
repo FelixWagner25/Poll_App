b08a6dda62a6207c3a953959c0e52e130239be98
Felix Wagner <post@felixwagner.eu>
2026-08-02 19:41:56 +0200

    feat: filter and render ending soon surveys into home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SurveyService } from '../shared/services/survey.service';
 import { Category } from '../shared/types/category';
+import { Survey } from '../shared/interfaces/survey';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +13,17 @@ import { Category } from '../shared/types/category';
 export class HomeComponent {
   dropdownOpened = signal<boolean>(false);
   selectedCategory: Category = null;
+  filteredSurveys = signal<Survey[]>([]);
 
-  surveyService = inject(SurveyService);
+  readonly surveyService = inject(SurveyService);
 
-  constructor() {}
+  endingSoonSurveys = computed(() => {
+    return this.surveyService.surveyList().filter((survey) => {
+      if (!survey.endDate) return false;
+      const remainingDays = this.calcDateDiffDays(this.getTodaysShortISOString(), survey.endDate);
+      return remainingDays >= 0 && remainingDays < 3;
+    });
+  });
 
   toggleDropdown() {
     this.dropdownOpened.update((value) => !value);
@@ -28,18 +36,27 @@ export class HomeComponent {
 
   getRemainingDaysString(endDate: string): string {
     if (endDate == '') return 'Active';
-    const endDateObj = new Date(endDate);
-    const today = new Date();
-    const diffInMs = endDateObj.getTime() - today.getTime();
-    const diffInDays = Math.ceil(diffInMs / (1000 * 60 * 60 * 24));
-    if (diffInDays < 0) {
+    const todayStr = this.getTodaysShortISOString();
+    const diffDays = this.calcDateDiffDays(todayStr, endDate);
+    if (diffDays < 0) {
       return 'Ended';
-    } else if (diffInDays == 0) {
+    } else if (diffDays == 0) {
       return 'Ends today';
-    } else if (diffInDays == 1) {
+    } else if (diffDays == 1) {
       return 'Ends in 1 day';
     } else {
-      return 'Ends in ' + diffInDays.toString() + 'days';
+      return 'Ends in ' + diffDays.toString() + ' days';
     }
+  }
+
+  calcDateDiffDays(dateString1: string, dateString2: string): number {
+    const date1 = new Date(dateString1);
+    const date2 = new Date(dateString2);
+    return date2.getDay() - date1.getDay();
+  }
+
+  getTodaysShortISOString(): string {
+    let today = new Date();
+    return today.toISOString().substring(0, 10);
   }
 }
